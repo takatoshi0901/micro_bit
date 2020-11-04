35d43a96219d81ab0baea0863302935f402afcb8
takatoshi0901 <73929893+takatoshi0901@users.noreply.github.com>
2020-11-04 16:12:22 +0900

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    Display = Display / 2
+})
+input.onGesture(Gesture.Shake, function () {
+    Display = 1
+})
+input.onButtonPressed(Button.AB, function () {
+    Display = 1
+})
+input.onButtonPressed(Button.B, function () {
+    Display = Display * 2
+})
+let Display = 0
+Display = 1
+basic.forever(function () {
+    basic.showNumber(Display)
+})
